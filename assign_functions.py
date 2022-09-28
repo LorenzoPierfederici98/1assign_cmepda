4ad1a7975a91faf987cbe325ebf3ca8154cb595e
Lorenzo Pierfederici <l.pierfederici@studenti.unipi.it>
2022-09-28 22:25:22 +0200

    Diviso file funzioni e main. Numero righe e caratteri

---
+++ assign_functions.py
@@ -0,0 +1,71 @@
+
+import re
+import time
+import matplotlib.pyplot as plt
+
+
+def process(file_path):
+    """Apre e legge il file dato da input e stampa il grafico a barre delle frequenze delle lettere nel file.
+    
+    Stampa il numero di caratteri senza spazi e il numero di righe dell'inputi file.
+    """
+    print(f'Opening input file {file_path}...')
+    with open(file_path, 'r') as input_file:
+        text = input_file.read()
+        find_frequency(text)
+        totale = 0
+        r = 0
+        for riga in text:
+            totale = totale + number_lines(riga)
+            r = r+1
+
+    print(f'Il numero di caratteri senza spazi è {totale}')
+    print(f'Il numero di righe del testo è {r}')
+    print('Done.')
+
+def histogram(dictionary):
+    """Mostra l'istogramma dei valori ordinati presenti in un dizionario.
+
+    Usato nella funzione findefrequency.
+    """
+    plt.title('Istogramma delle frequenze relative')
+    plt.xlabel('Lettere')
+    plt.ylabel('Frequenze [%]')
+    dictionary = dictionary.items()
+    dictionary = sorted(dictionary)
+    x,y = zip(*dictionary)
+    plt.bar(x,y)
+    plt.savefig('freq.pdf')
+    plt.show()
+    
+
+
+def find_frequency(file):
+    """Trova la frequenza relativa delle lettere dell'input file e ne stampa il grafico a barre.
+    
+    Inoltre stampa in output il tempo necessario a trovare le frequenze relative.
+    
+    Usato nella funzione process.
+    """
+    start_time = time.time()
+    letter_count = {}
+    file = file.lower()
+    n = float(len(file))
+    for letter in file:
+        match = re.search("[a-z]", letter)
+        boolean = bool(match)
+        if boolean:
+            if letter in letter_count:
+                letter_count[letter] +=1
+            else : letter_count[letter] = 1
+    for letter in letter_count:
+        letter_count[letter] = float(letter_count[letter]*100/n)
+    print("--- tempo trascorso : %s s ---" % (time.time() - start_time))
+   # print(letter_count)
+    histogram(letter_count)
+
+def number_lines(s):
+    """Inserisce gli elementi di una stringa separati da spazi in una lista e ne restituisce la lunghezza.
+    """
+    lista = str.split(s)
+    return len(lista)
--- assignment.py
+++ assignment.py
@@ -16,73 +16,11 @@
 
 """First assignment for the CMEPDA course, 2022/23.
 """
-
 import argparse
-import re
-import time
-import matplotlib.pyplot as plt
-
-
-
-
-
-def process(file_path):
-    """Apre e legge il file dato da input e stampa il grafico a barre delle frequenze delle lettere nel file.
-    """
-    print(f'Opening input file {file_path}...')
-    with open(file_path, 'r') as input_file:
-        text = input_file.read()
-#    print(text)
-        find_frequency(text)
-    print('Done.')
-
-def histogram(dictionary):
-    """Mostra l'istogramma dei valori ordinati presenti in un dizionario.
-
-    Usato nella funzione findefrequency.
-    """
-    plt.title('Istogramma delle frequenze relative')
-    plt.xlabel('Lettere')
-    plt.ylabel('Frequenze [%]')
-    dictionary = dictionary.items()
-    dictionary = sorted(dictionary)
-    x,y = zip(*dictionary)
-    plt.bar(x,y)
-    plt.savefig('freq.pdf')
-    plt.show()
-    
-
-
-def find_frequency(file):
-    """Trova la frequenza relativa delle lettere dell'input file e ne stampa il grafico a barre.
-    
-    Inoltre stampa in output il tempo necessario a trovare le frequenze relative.
-    
-    Usato nella funzione process.
-    """
-    start_time = time.time()
-    letter_count = {}
-    file = file.lower()
-    n = float(len(file))
-    for letter in file:
-        match = re.search("[a-z]", letter)
-        boolean = bool(match)
-        if boolean:
-            if letter in letter_count:
-                letter_count[letter] +=1
-            else : letter_count[letter] = 1
-    for letter in letter_count:
-        letter_count[letter] = float(letter_count[letter]*100/n)
-    print("--- %s seconds ---" % (time.time() - start_time))
-   # print(letter_count)
-    histogram(letter_count)
+import assign_functions
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Il programma trova le frequenze relative delle lettere presenti nel file di testo dato come input, le rappresenta in un grafico a barre e stampa il tempo trascorso per trovarle.')
     parser.add_argument('infile', type=str, help='percorso all\'inputfile.')
     args = parser.parse_args()
-    process(args.infile)
-
-    
-    
-     
+    assign_functions.process(args.infile)
