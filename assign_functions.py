1d0db5e2b31a8ee343b0b8ca2b3082de4d227c8b
Lorenzo Pierfederici <l.pierfederici@studenti.unipi.it>
2022-10-03 16:44:04 +0200

    Update assign_functions.py

--- assign_functions.py
+++ assign_functions.py
@@ -1,3 +1,19 @@
+
+# Copyright (C) 2022 l.pierfederici@studenti.unipi.it
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging 
 import re
 import time
@@ -6,9 +22,8 @@ import matplotlib.pyplot as plt
 
 def process(file_path):
     """Apre e legge il file dato da input e stampa il grafico a barre delle frequenze delle lettere nel file.
-    
-    Stampa il numero di caratteri senza spazi e il numero di righe dell'inputi file.
-    """
+    Stampa il numero di caratteri senza spazi e il numero di righe dell'inputi file."""
+
     print(f'Opening input file {file_path}...')
     with open(file_path, 'r') as input_file:
         text = input_file.read()
@@ -17,20 +32,18 @@ def process(file_path):
         lines = input_file.readlines()
         print(lines)
         totale = 0
-        r = 0
+        r = len(lines)
         for riga in lines:
-            totale += number_lines(riga)
-            r += 1     
+            totale += number_lines(riga)    
 
-    print(f'Il numero di caratteri compresi gli spazi è {totale-2}')
+    print(f'Il numero di caratteri compresi gli spazi è {totale-2*(r-1)}')
     print(f'Il numero di righe del testo è {r}')
     print('Done.')
 
 def histogram(dictionary):
     """Mostra l'istogramma dei valori ordinati presenti in un dizionario.
+    Usato nella funzione findefrequency."""
 
-    Usato nella funzione findefrequency.
-    """
     plt.title('Istogramma delle frequenze relative')
     plt.xlabel('Lettere')
     plt.ylabel('Frequenze [%]')
@@ -43,11 +56,9 @@ def histogram(dictionary):
     
 def find_frequency(file):
     """Trova la frequenza relativa delle lettere dell'input file e ne stampa il grafico a barre.
-    
     Inoltre stampa in output il tempo necessario a trovare le frequenze relative.
-    
-    Usato nella funzione process.
-    """
+    Usato nella funzione process."""
+
     start_time = time.time()
     letter_count = {}
     file = file.lower()
@@ -65,7 +76,8 @@ def find_frequency(file):
     histogram(letter_count)
 
 def number_lines(strn):
-    """Inserisce gli elementi di una stringa separati da spazi in una lista e ne restituisce la lunghezza.
-    """
+    """Inserisce gli elementi di una stringa separati da spazi in una lista e ne restituisce 
+    la lunghezza."""
+
     lista = re.split(r'',strn)
     return len(lista)
